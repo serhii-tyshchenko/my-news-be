1f30cc34120053689c8ef9e506fba12471f090a2
Serhii Tyshchenko <serhii.tyshchenko84@gmail.com>
2023-04-03 12:12:58 +0300

    fixed null date

--- src/parse.js
+++ src/parse.js
@@ -13,26 +13,39 @@ const normalizeDate = (date) => {
   };
   const monthsMap = {
     січ: 'Jan',
+    янв: 'Jan',
     лют: 'Feb',
+    фев: 'Feb',
     бер: 'Mar',
     мар: 'Mar',
     кві: 'Apr',
+    апр: 'Apr',
     тра: 'May',
+    май: 'May',
     чер: 'Jun',
+    июн: 'Jun',
     лип: 'Jul',
+    июл: 'Jul',
     сер: 'Aug',
+    авг: 'Aug',
     вер: 'Sep',
+    сен: 'Sep',
     жов: 'Oct',
+    окт: 'Oct',
     лис: 'Nov',
+    ноя: 'Nov',
     гру: 'Dec',
+    дек: 'Dec',
   };
 
-  const arrayToRegex = (arr) => new RegExp(Object.keys(arr).join("|"), 'gi');
+  const arrayToRegex = (arr) => new RegExp(Object.keys(arr).join('|'), 'gi');
 
   const regexDays = arrayToRegex(daysMap);
   const regexMonths = arrayToRegex(monthsMap);
 
-  const newDate = date.replace(regexDays, (match) => daysMap[match]).replace(regexMonths, (match) => monthsMap[match]);
+  const newDate = date
+    .replace(regexDays, (match) => daysMap[match])
+    .replace(regexMonths, (match) => monthsMap[match]);
   return newDate;
 };
 
