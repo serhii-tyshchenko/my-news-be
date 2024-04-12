f8f531507e5e10de4c04385e79e6d2e68257e8c9
Serhii Tyshchenko <serhii.tyshchenko84@gmail.com>
2024-04-12 23:01:29 +0300

    Revert "removed unused providers"
    This reverts commit 108588cb22675a1bbefbf941363dab9666dc14a3.

--- src/common/constants.js
+++ src/common/constants.js
@@ -3,16 +3,147 @@ const CATEGORY_ID = {
   TECH: 'tech',
   SOFTWARE_DEVELOPMENT: 'software_development',
   SCIENCE: 'science',
+  MILITARY: 'military',
   SPORT: 'sport',
   BUSINESS: 'business',
 };
 
 const LANGUAGE = {
   UK: 'uk',
+  RU: 'ru',
   EN: 'en',
 };
 
 const PROVIDERS_NEWS = [
+  {
+    id: 'censor',
+    type: 'rss',
+    name: 'Цензор.НЕТ',
+    description:
+      'Найсвіжіші огляди головних подій, резонансні статті, все про війну в Україні, авторські думки, фото та відео з місця подій, бізнес, розваги, політика, спорт.',
+    url: 'https://static.censor.net/censornet/rss/rss_uk_news.xml',
+    homepage: 'https://censor.net.ua',
+    logo: 'https://static.censor.net/censornet_mobile/images/logo/uk/favicon-bg.svg',
+    category: CATEGORY_ID.NEWS,
+    language: LANGUAGE.UK,
+  },
+  {
+    id: 'lb',
+    type: 'rss',
+    name: 'Lb.ua',
+    description: 'Лівий берег – останні новини дня в Україні та світі.',
+    url: 'https://lb.ua/rss/ukr/news.xml',
+    homepage: 'https://lb.ua',
+    logo: 'https://lb.ua/favicon.ico',
+    category: CATEGORY_ID.NEWS,
+    language: LANGUAGE.UK,
+  },
+  {
+    id: 'nv',
+    type: 'rss',
+    name: 'НВ',
+    description:
+      'Останні новини України та світу онлайн. Свіжі українські та світові новини політики, бізнесу, фінансів, технологій, стилю, спорту і важливих подій.',
+    url: 'https://nv.ua/ukr/rss/all.xml',
+    homepage: 'https://nv.ua',
+    logo: 'https://nv.ua/favicon/android-icon-36x36.png?new',
+    category: CATEGORY_ID.NEWS,
+    language: LANGUAGE.UK,
+  },
+  {
+    id: 'espreso',
+    type: 'rss',
+    name: 'Еспресо',
+    description:
+      'Найважливіші новини дня, свіжа інформація про війну з Росією, події на фронті...',
+    url: 'https://espreso.tv/rss',
+    homepage: 'https://espreso.tv',
+    logo: 'https://espreso.tv/favicon.ico',
+    category: CATEGORY_ID.NEWS,
+    language: LANGUAGE.UK,
+  },
+  {
+    id: 'radiosvoboda',
+    type: 'rss',
+    name: 'Радіо Свобода',
+    description: 'Сайт новин України та світу, відео, репортажів та аналітики.',
+    url: 'https://www.radiosvoboda.org/api/zrqiteuuir',
+    homepage: 'https://www.radiosvoboda.org',
+    logo: 'https://www.radiosvoboda.org/Content/responsive/RFE/img/webApp/favicon.ico',
+    category: CATEGORY_ID.NEWS,
+    language: LANGUAGE.UK,
+  },
+  {
+    id: 'unian',
+    type: 'rss',
+    name: 'УНІАН',
+    description:
+      'Новини політики, економіки, свіжі новини спорту, новини України та світу цілодобово українською мовою від УНІАН.',
+    url: 'https://rss.unian.net/site/news_ukr.rss',
+    homepage: 'https://www.unian.ua',
+    logo: 'https://www.unian.ua/favicon.ico',
+    category: CATEGORY_ID.NEWS,
+    language: LANGUAGE.UK,
+  },
+  {
+    id: 'obozrevatel',
+    type: 'rss',
+    name: 'Обозреватель',
+    description: 'Найсвіжіші новини України та світу онлайн.',
+    url: 'https://www.obozrevatel.com/ukr/out/rss/lastnews.xml',
+    homepage: 'https://www.obozrevatel.com',
+    logo: 'https://cdn.obozrevatel.com/news/img/favicons/favicon.ico',
+    category: CATEGORY_ID.NEWS,
+    language: LANGUAGE.UK,
+  },
+  {
+    id: 'liga',
+    type: 'rss',
+    name: 'Ліга.net',
+    description:
+      'Війна Росії проти України: онлайн-хроніка, останні події на Донбасі. Головні новини України та світу на сайті ЛІГА.net',
+    url: 'https://www.liga.net/news/all/rss.xml',
+    homepage: 'https://www.liga.net',
+    logo: 'https://www.liga.net/design/images/favicon.ico',
+    category: CATEGORY_ID.NEWS,
+    language: LANGUAGE.RU,
+  },
+  {
+    id: 'rbc',
+    type: 'rss',
+    name: 'РБК-Україна',
+    description:
+      "Головні новини України та світу сьогодні, політичні та аналітичні статті, новини економіки та курси валют, інтерв'ю, точки зору, новини життя на порталі РБК-Україна.",
+    url: 'https://www.rbc.ua/static/rss/all.ukr.rss.xml',
+    homepage: 'https://www.rbc.ua',
+    logo: 'https://www.rbc.ua/static/news/imgs/favicon.ico',
+    category: CATEGORY_ID.NEWS,
+    language: LANGUAGE.UK,
+  },
+  {
+    id: 'gordon',
+    type: 'rss',
+    name: 'Гордон',
+    description:
+      'Головні новини України і світу, ексклюзивні коментарі, аналітика.',
+    url: 'https://gordonua.com/ukr/xml/rss_category/politics.html',
+    homepage: 'https://gordonua.com',
+    logo: 'https://gordonua.com/favicon.ico',
+    category: CATEGORY_ID.NEWS,
+    language: LANGUAGE.UK,
+  },
+  {
+    id: 'tsn',
+    type: 'rss',
+    name: 'ТСН',
+    description:
+      'Всі новини за останню добу. Найактуальніші новини 1+1 онлайн, політики, економіки, стилю та спорту на TSN.ua',
+    url: 'https://tsn.ua/rss/full.rss',
+    homepage: 'https://tsn.ua',
+    logo: 'https://tsn.ua/favicon.svg',
+    category: CATEGORY_ID.NEWS,
+    language: LANGUAGE.UK,
+  },
   {
     id: 'bbc_world',
     type: 'rss',
@@ -24,6 +155,43 @@ const PROVIDERS_NEWS = [
     logo: 'https://www.bbc.com/favicon.ico',
     category: CATEGORY_ID.NEWS,
     language: LANGUAGE.EN,
+  },
+];
+
+const PROVIDERS_MILITARY = [
+  {
+    id: 'armyinform',
+    type: 'rss',
+    name: 'АрміяInform',
+    description: 'Інформаційне агентство АрміяInform',
+    url: 'https://armyinform.com.ua/feed/',
+    homepage: 'https://armyinform.com.ua',
+    logo: 'https://armyinform.com.ua/favicon.ico',
+    category: CATEGORY_ID.MILITARY,
+    language: LANGUAGE.UK,
+  },
+  {
+    id: 'militarnyj',
+    type: 'rss',
+    name: 'Мілітарний портал',
+    description: 'Чесно про армію, війну та оборонку з 2009 року',
+    url: 'https://mil.in.ua/uk/news/feed/',
+    homepage: 'https://mil.in.ua',
+    logo: 'https://mil.in.ua/wp-content/themes/military/favicon.png',
+    category: CATEGORY_ID.MILITARY,
+    language: LANGUAGE.UK,
+  },
+  {
+    id: 'defense_express',
+    type: 'rss',
+    name: 'Defense Express',
+    description:
+      'Добірка найсвіжіших новин та аналітики по військовій тематиці. Статті експертів, описи озброєнь та багато іншого.',
+    url: 'https://defence-ua.com/rss/feed.xml',
+    homepage: 'https://defence-ua.com',
+    logo: 'https://defence-ua.com/images/favicon.png',
+    category: CATEGORY_ID.MILITARY,
+    language: LANGUAGE.UK,
   },
 ];
 
@@ -103,6 +271,18 @@ const PROVIDERS_SOFTWARE_DEVELOPMENT = [
     language: LANGUAGE.EN,
   },
   {
+    id: 'dou',
+    type: 'rss',
+    name: 'DOU',
+    description:
+      'Новини, статті, дайджести, блоги, тренди, форум – для всіх, хто цікавиться розробкою програмного забезпечення.',
+    url: 'https://dou.ua/feed/',
+    homepage: 'https://dou.ua',
+    logo: 'https://s.dou.ua/assets/img/icon.png',
+    category: CATEGORY_ID.SOFTWARE_DEVELOPMENT,
+    language: LANGUAGE.UK,
+  },
+  {
     id: 'freecodecamp',
     type: 'rss',
     name: 'freeCodeCamp',
@@ -117,6 +297,29 @@ const PROVIDERS_SOFTWARE_DEVELOPMENT = [
 ];
 
 const PROVIDERS_BUSINESS = [
+  {
+    id: 'bbc_science',
+    type: 'rss',
+    name: 'BBC Science',
+    description:
+      'The latest BBC Science and Environment News: breaking news, analysis and debate on science and nature in the UK and around the world.',
+    url: 'http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/sci/tech/rss.xml',
+    homepage: 'https://www.bbc.com/news/science_and_environment',
+    logo: 'https://www.bbc.com/favicon.ico',
+    category: CATEGORY_ID.SCIENCE,
+    language: LANGUAGE.EN,
+  },
+  {
+    id: 'censor_business',
+    type: 'rss',
+    name: 'Бізнес.Цензор',
+    description: 'Найсвіжіші новини про бізнес та економіку',
+    url: 'https://static.censor.net/biz/rss/rss_uk_news.xml',
+    homepage: 'https://biz.censor.net',
+    logo: 'https://static.censor.net/biz/images/logo/uk/favicon-bg.svg',
+    category: CATEGORY_ID.BUSINESS,
+    language: LANGUAGE.UK,
+  },
   {
     id: 'bbc_business',
     type: 'rss',
@@ -133,18 +336,6 @@ const PROVIDERS_BUSINESS = [
 
 const PROVIDERS_SCIENCE = [
   {
-    id: 'bbc_science',
-    type: 'rss',
-    name: 'BBC Science',
-    description:
-      'The latest BBC Science and Environment News: breaking news, analysis and debate on science and nature in the UK and around the world.',
-    url: 'http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/sci/tech/rss.xml',
-    homepage: 'https://www.bbc.com/news/science_and_environment',
-    logo: 'https://www.bbc.com/favicon.ico',
-    category: CATEGORY_ID.SCIENCE,
-    language: LANGUAGE.EN,
-  },
-  {
     id: 'spacecom',
     type: 'rss',
     name: 'Space.com',
@@ -160,6 +351,7 @@ const PROVIDERS_SCIENCE = [
 
 const PROVIDERS = [
   ...PROVIDERS_NEWS,
+  ...PROVIDERS_MILITARY,
   ...PROVIDERS_TECH,
   ...PROVIDERS_SOFTWARE_DEVELOPMENT,
   ...PROVIDERS_BUSINESS,
